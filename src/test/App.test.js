2ce06226c6bbbc6ab2c4b15d4da4734024bf250d
Jhen Nunez <jxelektro@gmail.com>
2022-09-22 12:24:08 -0300

    9. Omitido( F()undef.) - Identado - AppScope

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import React from 'react';
+
+
 
 test('Renderiza en Pantalla', () => {
   render(<App />);
-      const startCount = screen.getByText("You clicked 0 times");
+  const startCount = screen.getByText("You clicked 0 times");
   expect(startCount).toBeTruthy();
 });
