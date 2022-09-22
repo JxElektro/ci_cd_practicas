b406bcbf385d8d2bcb61860b89860c653d8d1ef2
Jhen Nunez <jxelektro@gmail.com>
2022-09-22 00:00:29 -0300

    Correcion

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -4,5 +4,5 @@ import App from '../App';
 test('Renderiza en Pantalla', () => {
   render(<App />);
   const startCount = screen.getByText("You clicked 0 times");
-  expect(startCount).toBeNull(); 
+  expect(startCount).toBeTruthy(); 
 });
