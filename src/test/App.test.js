167fd63f9f9af08755818f158850ff26d9ba47e2
Jhen Nunez <jxelektro@gmail.com>
2022-09-21 22:13:07 -0300

    8try

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
+test('Renderiza en Pantalla', () => {
   render(<App />);
   const startCount = screen.queryByTestId("click");
   expect(startCount).toBeDefined()
